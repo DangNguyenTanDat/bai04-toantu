3bf8d056dd59ac1d6dcc85259c873c3707b2c545
DangNguyenTanDat <156651386+DangNguyenTanDat@users.noreply.github.com>
2024-01-15 16:54:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+    $a = 10;
+    $b = 2;
+    $tong = $a == $b;
+    echo "Tong: $tong <br>";
+
+    $hieu = $a - $b;
+    echo "Hieu: $hieu<br>";
+
+    $tich = $a * $b;
+    echo "Tich: $tich <br>";
+
+    $thuong = $a / $b;
+    echo "Thuong: $thuong <br>";
+
+    $sodu = $a % $b;
+    echo "So du: $sodu <br>";
+
+    $c = 20;
+    $d = 5;
+    $ss_bang = $c == $d;
+    echo "So sanh: $ss_bang <br>";
+
+    $ss_khac = $c != $d;
+    echo "So sanh khac: $ss_khac <Br>";
+
+    $ss_lon_hon = $c > $d;
+    echo "So sanh lon hon : $ss_lon_hon <br>";
+
+    $e = 10;
+    $e --; //$a = $a -1;
+    $f = 2;
+    $e += $f;
+
+?>
